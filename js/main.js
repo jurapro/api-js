75da0ba37f9bfac57772674d0cc0c3198569e121
jurapro <jurapro@yandex.ru>
2021-02-09 22:26:01 +0700

    Added showcase

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const host = 'http://localhost/api';
 
-const f = async (url, method='get', data = [], token = null) => {
+const f = async (url, method = 'get', data = [], token = null) => {
     const options = {
         method: method.toUpperCase(),
         headers: {
@@ -8,16 +8,49 @@ const f = async (url, method='get', data = [], token = null) => {
         },
     }
 
-    if(token)
+    if (token)
         options.headers['Authorization'] = `Bearer ${token}`
 
-    if(['post', 'patch'].includes(method))
+    if (['post', 'patch'].includes(method))
         options.body = JSON.stringify(data)
 
     return await fetch(`${host}/${url}`, options).then(res => res.json());
 }
 
-async function click() {
-    const res = await f('products');
-    console.log(res);
+class Product {
+    constructor(product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.description = product.description;
+        this.price = product.price;
+    }
+
+    getTemplate() {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = `
+            <p>${this.name} - ${this.price} руб.</p>
+            <hr>
+            <p>${this.description}</p>`;
+        return div;
+    }
 }
+
+class Showcase {
+    constructor() {
+        this.$html = document.querySelector('.showcase');
+        this.products = [];
+        this.loadProducts().then(() => this.render());
+    }
+
+    async loadProducts() {
+        let list = await f('products');
+        list.forEach(el => this.products.push(new Product(el)));
+    }
+
+    render() {
+        this.products.forEach(el => this.$html.append(el.getTemplate()));
+    }
+}
+
+const showcase = new Showcase();
