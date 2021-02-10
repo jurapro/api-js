e683532271fd66cc7ecac3c32e6295bbafbc7142
jurapro <jurapro@yandex.ru>
2021-02-10 22:40:55 +0700

    Added login and logout user

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ class Product {
         this.name = product.name;
         this.description = product.description;
         this.price = product.price;
+        this.$html = this.getTemplate();
     }
 
     getTemplate() {
@@ -49,8 +50,108 @@ class Showcase {
     }
 
     render() {
-        this.products.forEach(el => this.$html.append(el.getTemplate()));
+        this.$html.innerHTMl = '';
+        this.products.forEach(el => this.$html.append(el.$html));
     }
 }
 
-const showcase = new Showcase();
+class LoginForm {
+    constructor() {
+        this.data = {
+            email: '',
+            password: '',
+            api_token: ''
+        };
+        this.$html = document.querySelector('.profile');
+        this.render();
+        this.bindEvents();
+    }
+
+
+    bindEvents() {
+
+        document.addEventListener('user-login', e => {
+            this.data.api_token = e.detail.api_token;
+            this.render();
+        });
+
+        document.addEventListener('user-out', () => {
+            this.data.api_token = '';
+            this.render();
+        });
+    }
+
+    render() {
+        this.$html.innerHTML = '';
+        if (this.data.api_token) {
+            this.$html.append(this.getTemplateOut());
+        } else {
+            this.$html.append(this.getTemplate());
+        }
+    }
+
+    inputText(e) {
+        this.data[e.target.dataset.model] = e.target.value;
+    }
+
+    getTemplate() {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = `
+            <h3>Вход</h3>
+            <div class="message"></div>
+            <label>Логин: <input type="email" data-model="email"></label>            
+            <label>Пароль: <input type="password" data-model="password"></label>  
+            <button>Вход</button>          
+            `;
+        div.querySelectorAll('input')
+            .forEach(el => el.addEventListener('input', (e) => this.inputText(e)));
+
+        div.querySelector('button')
+            .addEventListener('click', () => this.login());
+        return div;
+    }
+
+    getTemplateOut() {
+        let div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = `
+            <h3>Вы вошли как ${this.data.email}</h3> 
+            <button>Выход</button>          
+            `;
+        div.querySelector('button')
+            .addEventListener('click', () => this.out());
+        return div;
+    }
+
+    async login() {
+        let res = await f('login', 'post', this.data);
+        if (res.message) {
+            this.$html.querySelector('.message').innerHTML = 'Не правильный логин или пароль';
+        } else {
+            document.dispatchEvent(new CustomEvent(
+                'user-login', {
+                    detail: {email: this.data.email, api_token: res.api_token}
+                }
+            ));
+        }
+    }
+
+    async out() {
+        if (!this.data.api_token) return;
+        let res = await f('logout', 'post', this.data, this.data.api_token);
+        if (!res.message) {
+            document.dispatchEvent(new CustomEvent('user-out'));
+        }
+    }
+
+}
+
+class App {
+    constructor() {
+        this.showcase = new Showcase();
+        this.loginForm = new LoginForm();
+    }
+}
+
+const app = new App();
